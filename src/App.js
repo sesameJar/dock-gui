92c496447b11d50c1f25489351a1ed6c50411c49
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-12-28 01:26:25 -0500

    generting dids

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import './App.css';
 
-import { useEffect, useState } from 'react'
+import {useEffect, useState} from 'react'
 //import components
 import CredentialFrom from "./components/CredentialForm"
 import PresentationFrom from "./components/PresentationForm"
 // Constants
 import { address, secretUri, secretUri2, secretUri3, secretUri4 } from './utils/shared-constants';
 // Dock and Polkadot neede modules
+import Keyring from '@polkadot/keyring';
+import { PublicKeySr25519 } from '@docknetwork/sdk';
+import { createKeyDetail, createNewDockDID } from '@docknetwork/sdk/utils/did';
+
+import getKeyDoc from "@docknetwork/sdk/utils/vc/helpers.js"
+
+// import {connectToNode} from "./utils/dockSdkInit"
 import { DockAPI } from '@docknetwork/sdk';
+import VerifiableCredential from '@docknetwork/sdk/verifiable-credential';
+import VerifiablePresentation from "@docknetwork/sdk/verifiable-presentation"
 
 function App() {
   const [loading, setLoading] = useState(false)
    //Initialize a new custom dock API
   const [dock, setDock] = useState(new DockAPI())
+  const [credentials, setCredentials] = useState([]);
+  const [presentations, setPresentations] = useState([]);
+  const [authorityDID, setAuthorityDID] = useState()
+  const [holdersDID, setHoldersDID] = useState([])
 
-  useEffect(() => { 
-    async function initDock() { 
-      console.log('initializing the Dock Api')
+  useEffect(() => {
+    async function initDock (){
+      setLoading(true)
+      if (!dock.isInitialized() && !dock.isConnected) {
+        console.log('initializing the Dock Api')
         await dock.init({address, keyring:Keyring})
         //Setting Alice as the issuer authority
         const issuer = dock.keyring.addFromUri(secretUri, null, 'sr25519');
         dock.setAccount(issuer);
+
+        const issuerDid = await generateDID(secretUri)
+        setAuthorityDID(issuerDid)
+        console.log("DID", issuerDid)
+        
+        //Mock Holder1
+        const holder2DID = await generateDID(secretUri2)
+        console.log("DID holder", holder2DID)
+        //Mock Holder2
+        // const holder3DID = await generateDID(secretUri3)
+        // console.log("DID holder", holder3DID)
+        // //Mock Holder3
+        // const holder4DID = await generateDID(secretUri4)
+        // console.log("DID holder", holder4DID)
+
+        // setHoldersDID([holder2DID, holder3DID, holder4DID])
+        setHoldersDID([holder2DID])
+        // Registering all DIDs for future verification
+
+        setLoading(false)
+       }
     }
     initDock()
     return  () => {
@@ -28,7 +64,20 @@ function App() {
       // disconnect from the node(WIP)
       dock.disconnect()
     }
-  })
+   }, [dock])
+  
+  async function generateDID(secret) {
+    const did = createNewDockDID();
+
+    const account = dock.keyring.addFromUri(secret, null, 'sr25519');
+    const pk = PublicKeySr25519.fromKeyringPair(account);
+    
+    const keyDetail = createKeyDetail(pk, did);
+    await dock.did.new(did, keyDetail);
+
+    return {did, keyPair: account }
+    // return did
+  }
 
  // handler for AddCredential Event 
   const handleNewCredential = async credentialObj => { 
@@ -56,8 +105,8 @@ function App() {
     } else {
       return (
         <>
-          <CredentialFrom onAddCredential={ handleNewCredential}/>
-          <PresentationFrom onAddPresentation={handleNewPresentation} />
+          <CredentialFrom holders={holdersDID} onAddCredential={ handleNewCredential}/>
+          <PresentationFrom holders={holdersDID} credentials={credentials} onAddPresentation={handleNewPresentation} />
         </>
       )
     }
