6c11a29904b28d64cc6e2931504826ef82644f71
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-12-28 00:43:51 -0500

    initializing node connection

--- src/App.js
+++ src/App.js
@@ -4,12 +4,37 @@ import { useEffect, useState } from 'react'
 //import components
 import CredentialFrom from "./components/CredentialForm"
 import PresentationFrom from "./components/PresentationForm"
+// Constants
+import { address, secretUri, secretUri2, secretUri3, secretUri4 } from './utils/shared-constants';
+// Dock and Polkadot neede modules
+import { DockAPI } from '@docknetwork/sdk';
 
 function App() {
   const [loading, setLoading] = useState(false)
+   //Initialize a new custom dock API
+  const [dock, setDock] = useState(new DockAPI())
+
+  useEffect(() => { 
+    async function initDock() { 
+      console.log('initializing the Dock Api')
+        await dock.init({address, keyring:Keyring})
+        //Setting Alice as the issuer authority
+        const issuer = dock.keyring.addFromUri(secretUri, null, 'sr25519');
+        dock.setAccount(issuer);
+    }
+    initDock()
+    return  () => {
+      console.log("App unmounting")
+      // disconnect from the node(WIP)
+      dock.disconnect()
+    }
+  })
 
  // handler for AddCredential Event 
-  const handleNewCredential = async credentialObj => {}
+  const handleNewCredential = async credentialObj => { }
+  
+  // handler for AddPresentation Event 
+  const handleNewPresentation = async presentationObj => { }
 
   function Render() {
     if (loading) {
@@ -17,8 +42,8 @@ function App() {
     } else {
       return (
         <>
-          <CredentialFrom />
-          <PresentationFrom />
+          <CredentialFrom onAddCredential={ handleNewCredential}/>
+          <PresentationFrom onAddPresentation={handleNewPresentation} />
         </>
       )
     }
---
+++ src/utils/shared-constants.js
@@ -0,0 +1,3 @@
+export const address = 'ws://localhost:9944'; // Websocket address of your Dock node
+export const secretUri = '//Alice'; // Account secret in uri format, we will use Alice for local testing
+
